db38a0e357f6eadd6c8c6ac4686ce9b775a9c10d
David Nguyen <david.nguyen11@student.csulb.edu>
2016-10-18 12:45:16 -0700

    Front end initial

--- fitBit GO/www/js/app.js
+++ fitBit GO/www/js/app.js
@@ -4,11 +4,39 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 var app = angular.module('ionicApp', ['ionic'])
+
 app.controller('MainCtrl', function($scope, $ionicSideMenuDelegate) {
     $scope.toggleLeft = function() {
         $ionicSideMenuDelegate.toggleLeft()
     }
-})
+});
+
+app.controller('MapController', function($scope, $ionicLoading) {
+ 
+    google.maps.event.addDomListener(window, 'load', function() {
+        var myLatlng = new google.maps.LatLng(33.7838235, -118.1140904);
+ 
+        var mapOptions = {
+            center: myLatlng,
+            zoom: 14,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+ 
+        var map = new google.maps.Map(document.getElementById("map"), mapOptions); 
+        navigator.geolocation.getCurrentPosition(function(pos) {
+            map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+            var myLocation = new google.maps.Marker({
+                position: new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude),
+                map: map,
+                title: "My Location"
+            });
+        }
+        );
+ 
+        $scope.map = map;
+    });
+ 
+});
 
 
 
