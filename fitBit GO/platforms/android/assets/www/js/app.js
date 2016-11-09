bbb887aced44d0bd56c20c6765f7705613a9f892
Mark Levie Mendoza <mark@Marks-MacBook-Air.local>
2016-11-09 11:30:25 -0800

    added cdn for chart.js

--- fitBit GO/platforms/android/assets/www/js/app.js
+++ fitBit GO/platforms/android/assets/www/js/app.js
@@ -3,46 +3,73 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-var app = angular.module('ionicApp', ['ionic'])
+var app = angular.module('ionicApp', ['ionic', 'ui.router']);
 
-app.controller('MainCtrl', function($scope, $ionicSideMenuDelegate) {
-    $scope.toggleLeft = function() {
-        $ionicSideMenuDelegate.toggleLeft()
-    }
-});
+app.controller('MapCtrl', function ($scope, $ionicLoading) {
 
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
-app.controller('LoginCtrl', function($scope) {
-	$scope.show = true;
-});
-
-=======
->>>>>>> origin/master
->>>>>>> Stashed changes
-app.controller('MapController', function($scope, $ionicLoading) {
- 
-    google.maps.event.addDomListener(window, 'load', function() {
+    google.maps.event.addDomListener(window, 'load', function () {
         var myLatlng = new google.maps.LatLng(33.7838235, -118.1140904);
- 
+
         var mapOptions = {
             center: myLatlng,
             zoom: 14,
             mapTypeId: google.maps.MapTypeId.ROADMAP
         };
- 
-        var map = new google.maps.Map(document.getElementById("map"), mapOptions); 
-        navigator.geolocation.getCurrentPosition(function(pos) {
+
+        var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+        navigator.geolocation.getCurrentPosition(function (pos) {
             map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
             var myLocation = new google.maps.Marker({
                 position: new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude),
                 map: map,
                 title: "My Location"
             });
-        }
-        );
+        });
         $scope.map = map;
     });
 });
 
+app.config(function ($stateProvider, $urlRouterProvider) {
+
+    $stateProvider
+        .state('list', {
+            url: '/',
+            templateUrl: 'list.html',
+            controller: 'ListCtrl'
+        })
+        .state('view', {
+            url: '/movie/:movieid',
+            templateUrl: 'settings.html',
+            controller: 'ViewCtrl'
+        })
+        .state('view2', {
+            url: '/movie2/:movieid2',
+            templateUrl: 'profile.html',
+            controller: 'ViewCtrl'
+        });
+
+    $urlRouterProvider.otherwise("/");
+
+});
+
+app.controller('ListCtrl', function ($scope, $state) {
+    $scope.changePage = function () {
+        $state.go('view', {
+            movieid: 1
+        });
+        $state.go('view2', {
+            movieid: 1
+        });
+    }
+});
+
+app.controller('ViewCtrl', function ($scope, $stateParams, $ionicHistory) {
+    console.log($stateParams.movieid);
+    $scope.goBack = function () {
+        $ionicHistory.goBack();
+    }
+});
+
+app.controller('LoginCtrl', function ($scope) {
+    $scope.show = true;
+});
